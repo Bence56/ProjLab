e4ebdd6c565fe36985163f41b70b48978d62949f
Peti <abordanpeter@gmail.com>
2020-03-25 14:12:42 +0100

    Added new NEWTESTCLASS

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("helo");
+        System.out.println("Nem helo");
+        System.out.println("helo222");
     }
 }
