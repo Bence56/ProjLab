387f7ae47d2a17c1aa637a216daba57809454d25
Bence56 <bsfcb002@gmail.com>
2020-03-25 12:34:49 +0100

    Heartless dalszöveg

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	System.out.println("Never need a bitch. im what a bitch need");
     }
 }
