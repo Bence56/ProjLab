f7bd737e3aa5bc14ae9ef62398c3cab6d435b717
Peti <abordanpeter@gmail.com>
2020-03-25 12:36:40 +0100

    asdasdasasd

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Test");
+        System.out.println("Testa");
 	// write your code here
     }
 }
