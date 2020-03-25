05464487592c087a0174f834f01959dd7a6c9ee0
Peti <abordanpeter@gmail.com>
2020-03-25 12:57:49 +0100

    Hozzáadtam a .gitignore fájlt

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	System.out.println("Never need a bitch. im what a bitch need");
+
+	System.out.println("Never need a bitch. im what a bitch need");
     }
 }
