892fc6eb00de02c02ea14247ab48a4ed2e9eceff
erdei.emerencia <erdei.emerencia@gmail.com>
2020-04-25 16:48:16 +0200

    tesztek

--- Tests/com/company/PalyaTest.java
+++ Tests/com/company/PalyaTest.java
@@ -6,6 +6,7 @@ import org.opentest4j.AssertionFailedError;
 
 import java.io.IOException;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class PalyaTest {
@@ -27,6 +28,42 @@ class PalyaTest {
         try {
             parser.palyaParse(kontroller, "palya.json");
             System.out.println("A");
+
+           //13as mezőről a kutató lép, aztán kapar de ott nincs tárgy, továbblép és megint kapar ott van egy alkatrész
+            Jatekos kutato2 = kontroller.getJatekosok().get(2);
+            kutato2.lep(Irany.Bal);
+            try {
+                assertEquals(kutato2.getTartozkodasiMezo(),kontroller.palya.get(11));
+                System.out.println(ANSI_GREEN + "Siker" + ANSI_RESET);
+                kutato2.state();
+            } catch (AssertionFailedError e) {
+                System.out.println(ANSI_RED + "Fail: Nem jó helyre lép" + ANSI_RESET);
+            }
+            kutato2.kapar();
+            try {
+                assertTrue(kutato2.getTargyak().size()==0);
+                System.out.println(ANSI_GREEN + "Siker" + ANSI_RESET);
+                kutato2.state();
+            } catch (AssertionFailedError e) {
+                System.out.println(ANSI_RED + "Fail" + ANSI_RESET);
+            }
+            kutato2.lep(Irany.BalFel);
+            try {
+                assertEquals(kutato2.getTartozkodasiMezo(),kontroller.palya.get(8));
+                System.out.println(ANSI_GREEN + "Siker" + ANSI_RESET);
+                kutato2.state();
+            } catch (AssertionFailedError e) {
+                System.out.println(ANSI_RED + "Fail: Nem jó helyre lép" + ANSI_RESET);
+            }
+            kutato2.kapar();
+            try {
+                assertTrue(kutato2.getTargyak().size()==1);
+                System.out.println(ANSI_GREEN + "Siker" + ANSI_RESET);
+                kutato2.state();
+            } catch (AssertionFailedError e) {
+                System.out.println(ANSI_RED + "Fail" + ANSI_RESET);
+            }
+
 
         } catch (IOException e) {
             e.printStackTrace();
