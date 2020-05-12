842798726e12aeb65caf1c186f47a57a44cfe122
Enikő Erős <eeros@edu.bme.hu>
2020-05-13 01:19:59 +0200

    Merge branch 'master' of C:\Users\Pitz\IdeaProjects\Szoftprojlab with conflicts.

--- src/com/company/FunkciokPanel.java
+++ src/com/company/FunkciokPanel.java
@@ -1,4 +1,4 @@
-package com.company;
+/*package com.company;
 
 import javax.swing.*;
 import java.awt.*;
@@ -6,10 +6,14 @@ import java.awt.*;
 public class FunkciokPanel extends JPanel {
     JButton kapar=new JButton("Kapar");
     JButton osszeszerel=new JButton("Összeszerel");
+    JButton iglutepit=new JButton("Iglut épít");
     JButton vizsgal=new JButton("Vizsgál");
+
+
     public FunkciokPanel(Kontroller kontroller){
 
         GridLayout gl3=new GridLayout(2,2);
+        //this.setLayout(gl3);
         this.setLayout(gl3);
         kapar.setActionCommand("kapar");
         kapar.addActionListener(kontroller);
@@ -20,13 +24,149 @@ public class FunkciokPanel extends JPanel {
         vizsgal.setActionCommand("vizsgal");
         vizsgal.addActionListener(kontroller);
         this.add(vizsgal);
-        JButton iglutepit=new JButton("Iglut epít");
         iglutepit.setActionCommand("iglut épít");
         iglutepit.addActionListener(kontroller);
         this.add(iglutepit);
     }
     //Egyéb cselekvési lehetőségek, állandóak.
 
+    public void update(Jatekos aktivJatekos){
+        revalidate();
+    }
+}
+*/
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+public class FunkciokPanel extends JPanel {
+    // Játékos cuccainak a panelja
+    JButton kapar=new JButton();
+    JButton osszeszerel=new JButton();
+    JButton iglutepit=new JButton();
+    JLabel vizsgal=new JLabel();
+
+
+
+
+    private JButton balfel=new JButton();
+    private JButton fel=new JButton();
+    private JButton bal=new JButton();
+    private JButton jobbfel=new JButton();
+
+    private JButton jobb=new JButton();
+    private JButton balle=new JButton();
+    private JButton le=new JButton();
+    private JButton jobble=new JButton();
+
+    private ImageIcon osszeszerelim=new ImageIcon("Resources/Assets/osszeszerel.png");
+    private ImageIcon nagyitoim=new ImageIcon("Resources/Assets/nagyito.png");
+    private ImageIcon x=new ImageIcon("Resources/Assets/x.png");
+    private ImageIcon csakanyim=new ImageIcon("Resources/Assets/csákány.png");
+    private ImageIcon igluim=new ImageIcon("Resources/Assets/Iglu_I-01_kisebb.png");
+
+    public FunkciokPanel(Kontroller kontroller){
+
+
+        JPanel targyak=new JPanel();
+        targyak.setPreferredSize(new Dimension(220,220));
+        GridLayout gl1=new GridLayout(2,2);
+        targyak.setLayout(gl1);
+
+
+        this.add(kapar);
+        //kapar.setPreferredSize(new Dimension(90,90));
+        kapar.setIcon(csakanyim);
+        targyak.add(kapar);
+        kapar.setActionCommand("kapar");
+        kapar.addActionListener(kontroller);
+
+        osszeszerel.setIcon(osszeszerelim);
+        targyak.add(osszeszerel);
+        osszeszerel.setActionCommand("osszeszerel");
+        osszeszerel.addActionListener(kontroller);
+
+        //Vizsgál panel irányonként egy gombbal
+        JPanel targyak2=new JPanel();
+        GridLayout gl5=new GridLayout(3,3);
+        targyak2.setLayout(gl5);
+
+
+        //BalFel
+        //balfel.setPreferredSize(new Dimension(60,60));
+        balfel.setIcon(x);
+        balfel.setActionCommand("balfentről");
+        balfel.addActionListener(kontroller);
+        targyak2.add(balfel);
+
+        //Fel
+        //  fel.setPreferredSize(new Dimension(60,60));
+        fel.setIcon(x);
+        fel.setActionCommand("fentről");
+        fel.addActionListener(kontroller);
+        targyak2.add(fel);
+
+        //JobbFel
+        //  jobbfel.setPreferredSize(new Dimension(60,60));
+        jobbfel.setIcon(x);
+        jobbfel.setActionCommand("jobbfentről");
+        jobbfel.addActionListener(kontroller);
+        targyak2.add(jobbfel);
+
+
+        //Bal
+        // bal.setPreferredSize(new Dimension(60,60));
+        bal.setIcon(x);
+        bal.setActionCommand("balról");
+        bal.addActionListener(kontroller);
+        targyak2.add(bal);
+
+        //kotel.setPreferredSize(new Dimension(60,60));
+        vizsgal.setIcon(nagyitoim);
+        targyak2.add(vizsgal);
+
+        //Jobb
+        //jobb.setPreferredSize(new Dimension(60,60));
+        jobb.setIcon(x);
+        jobb.setActionCommand("jobbról");
+        jobb.addActionListener(kontroller);
+        targyak2.add(jobb);
+
+
+        //BalLe
+        //balle.setPreferredSize(new Dimension(60,60));
+        balle.setIcon(x);
+        balle.setActionCommand("ballentről");
+        balle.addActionListener(kontroller);
+        targyak2.add(balle);
+
+        //Le
+        // le.setPreferredSize(new Dimension(60,60));
+        le.setIcon(x);
+        le.setActionCommand("lentről");
+        le.addActionListener(kontroller);
+        targyak2.add(le);
+
+        //jobble.setPreferredSize(new Dimension(60,60));
+        jobble.setIcon(x);
+        jobble.setActionCommand("jobblentről");
+        jobble.addActionListener(kontroller);
+        targyak2.add(jobble);
+        targyak.add(targyak2);
+
+
+        //Az alkatrész lerak úgy van megírva, hogy bármennyi alkatrészünk van, 1 lerak() hívással a 0. indexűt rakjuk le.  Itt 1 alkatrészt
+        iglutepit.setPreferredSize(new Dimension(90,90));
+        iglutepit.setIcon(igluim); //pisztolyNULL
+        iglutepit.setActionCommand("iglut epít");
+        iglutepit.addActionListener(kontroller);
+        targyak.add(iglutepit);
+
+        targyak.add(targyak2, BorderLayout.LINE_START);
+        this.add(targyak);
+    }
     public void update(Jatekos aktivJatekos){
         revalidate();
     }
--- src/com/company/TargyakPanel.java
+++ src/com/company/TargyakPanel.java
@@ -1,109 +1,86 @@
 package com.company;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Map;
 
 public class TargyakPanel extends JPanel {
     // Játékos cuccainak a panelja
-    Map<String, BufferedImage> img;
-
-    private JButton lapat=new JButton();
-    private JButton sator=new JButton();
-    private JButton alkatresz=new JButton();
-    private JLabel kotel=new JLabel();
+    private JButton lapat = new JButton();
+    private JButton sator = new JButton();
+    private JButton alkatresz = new JButton();
+    private JLabel kotel = new JLabel();
 
 
-    private JButton balfel=new JButton();
-    private JButton fel=new JButton();
-    private JButton bal=new JButton();
-    private JButton jobbfel=new JButton();
+    private JButton balfel = new JButton();
+    private JButton fel = new JButton();
+    private JButton bal = new JButton();
+    private JButton jobbfel = new JButton();
 
-    private JButton jobb=new JButton();
-    private JButton balle=new JButton();
-    private JButton le=new JButton();
-    private JButton jobble=new JButton();
+    private JButton jobb = new JButton();
+    private JButton balle = new JButton();
+    private JButton le = new JButton();
+    private JButton jobble = new JButton();
 
-    private ArrayList<BufferedImage> retegek=new ArrayList<BufferedImage>();
 
-    private BufferedImage lapatim;
-    private BufferedImage lapatimNULL;
-    private BufferedImage satorim;
-    private BufferedImage satorimNULL;
-    private BufferedImage x;
-    private BufferedImage kotelim;
-    private BufferedImage kotelimNULL;
+    private ImageIcon kotelim=new ImageIcon("Resources/Assets/Kotel_I-02_kisebb.png");
+    private ImageIcon kotelimNULL=new ImageIcon("Resources/Assets/Kotel_I_NULL-02_kisebb.png");
+    private ImageIcon lapatim = new ImageIcon("Resources/Assets/Lapat_I-01.png");
+    private ImageIcon lapatimNULL = new ImageIcon("Resources/Assets/Lapat_I_NULL-01.png");
+    private ImageIcon satorim = new ImageIcon("Resources/Assets/Sator_I-01_kisebb.png");
+    private ImageIcon satorimNULL = new ImageIcon("Resources/Assets/Sator_I-01_kisebb.png");
+    private ImageIcon x = new ImageIcon("Resources/Assets/x.png");
+    private ImageIcon kotelim1 = new ImageIcon("Resources/Assets/Kotel_I-02.png");
+    private ImageIcon kotelimNULL1 = new ImageIcon("Resources/Assets/Kotel_I_NULL-02.png");
+    private ImageIcon pisztolyegy = new ImageIcon("Resources/Assets/1-Pisztoly_I-01.png");
+    private ImageIcon pisztolyket = new ImageIcon("Resources/Assets/2-Pisztoly_I-01.png");
+    private ImageIcon pisztolyhar = new ImageIcon("Resources/Assets/3-Pisztoly_I-01.png");
+    private ImageIcon pisztolyNULL = new ImageIcon("Resources/Assets/Pisztoly_I_NULL-01.png");
 
-    private BufferedImage pisztolyNULL;
-    private BufferedImage pisztolyegy;
-    private BufferedImage pisztolyket;
-    private BufferedImage pisztolyhar;
-    public TargyakPanel(Kontroller kontroller,Map<String, BufferedImage> images){
+    public TargyakPanel(Kontroller kontroller) {
 
-       // BoxLayout box=new BoxLayout(this,2);
-       // this.setLayout(box);
-        try {
-            img = images;
-            lapatim = ImageIO.read(new File("Resources/Assets/Lapat_I-01.png"));
-            lapatimNULL = ImageIO.read(new File("Resources/Assets/Lapat_I_NULL-01.png"));
-            satorim = ImageIO.read(new File("Resources/Assets/Sator_I-01.png"));
-            satorimNULL = ImageIO.read(new File("Resources/Assets/Sator_I_NULL-01.png"));
-            x = ImageIO.read(new File("Resources/Assets/x.png"));
-            kotelim = ImageIO.read(new File("Resources/Assets/Kotel_I-02.png"));
-            kotelimNULL = ImageIO.read(new File("Resources/Assets/Kotel_I_NULL-02.png"));
-            pisztolyNULL = ImageIO.read(new File("Resources/Assets/Pisztoly_I_NULL-01.png"));
-            pisztolyegy = ImageIO.read(new File("Resources/Assets/1-Pisztoly_I-01.png"));
-            pisztolyket = ImageIO.read(new File("Resources/Assets/2-Pisztoly_I-01.png"));
-            pisztolyhar = ImageIO.read(new File("Resources/Assets/3-Pisztoly_I-01.png"));
-        }catch(IOException e){
-            e.printStackTrace();
-        }
-        retegek.add(pisztolyNULL);
-        JPanel targyak=new JPanel();
-        targyak.setPreferredSize(new Dimension(256,300));
-        GridLayout gl1=new GridLayout(2,2);
+
+        JPanel targyak = new JPanel();
+        targyak.setPreferredSize(new Dimension(220, 220));
+        GridLayout gl1 = new GridLayout(2, 2);
         targyak.setLayout(gl1);
 
-        lapat.setPreferredSize(new Dimension(100,100));
-        lapat.setIcon(new ImageIcon(lapatim));
+      //  lapat.setPreferredSize(new Dimension(80, 80));
+        lapat.setIcon((lapatim));
         targyak.add(lapat);
         lapat.setActionCommand("lapatol");
         lapat.addActionListener(kontroller);
 
 
-        sator.setPreferredSize(new Dimension(100,100));
-        sator.setIcon(new ImageIcon(satorim));
+      //  sator.setPreferredSize(new Dimension(80, 80));
+        sator.setIcon(satorim);
         targyak.add(sator);
         sator.setActionCommand("satrat epit");
         sator.addActionListener(kontroller);
-        JPanel targyak2=new JPanel();
-        GridLayout gl5=new GridLayout(3,3);
+
+        //Kihúz panel irányonként egy gombbal
+        JPanel targyak2 = new JPanel();
+        GridLayout gl5 = new GridLayout(3, 3);
         targyak2.setLayout(gl5);
 
 
         //BalFel
         //balfel.setPreferredSize(new Dimension(60,60));
-        balfel.setIcon(new ImageIcon(x));
+        balfel.setIcon(x);
         balfel.setActionCommand("balfentről");
         balfel.addActionListener(kontroller);
         targyak2.add(balfel);
 
         //Fel
         //  fel.setPreferredSize(new Dimension(60,60));
-        fel.setIcon(new ImageIcon(x));
+        fel.setIcon(x);
         fel.setActionCommand("fentről");
         fel.addActionListener(kontroller);
         targyak2.add(fel);
 
         //JobbFel
         //  jobbfel.setPreferredSize(new Dimension(60,60));
-        jobbfel.setIcon(new ImageIcon(x));
+        jobbfel.setIcon(x);
         jobbfel.setActionCommand("jobbfentről");
         jobbfel.addActionListener(kontroller);
         targyak2.add(jobbfel);
@@ -111,18 +88,18 @@ public class TargyakPanel extends JPanel {
 
         //Bal
         // bal.setPreferredSize(new Dimension(60,60));
-        bal.setIcon(new ImageIcon(x));
+        bal.setIcon(x);
         bal.setActionCommand("balról");
         bal.addActionListener(kontroller);
         targyak2.add(bal);
 
         //kotel.setPreferredSize(new Dimension(60,60));
-        kotel.setIcon(new ImageIcon(kotelim));
+        kotel.setIcon(kotelim);
         targyak2.add(kotel);
 
         //Jobb
         //jobb.setPreferredSize(new Dimension(60,60));
-        jobb.setIcon(new ImageIcon(x));
+        jobb.setIcon(x);
         jobb.setActionCommand("jobbról");
         jobb.addActionListener(kontroller);
         targyak2.add(jobb);
@@ -130,20 +107,20 @@ public class TargyakPanel extends JPanel {
 
         //BalLe
         //balle.setPreferredSize(new Dimension(60,60));
-        balle.setIcon(new ImageIcon(x));
+        balle.setIcon(x);
         balle.setActionCommand("ballentről");
         balle.addActionListener(kontroller);
         targyak2.add(balle);
 
         //Le
         // le.setPreferredSize(new Dimension(60,60));
-        le.setIcon(new ImageIcon(x));
+        le.setIcon(x);
         le.setActionCommand("lentről");
         le.addActionListener(kontroller);
         targyak2.add(le);
 
         //jobble.setPreferredSize(new Dimension(60,60));
-        jobble.setIcon(new ImageIcon(x));
+        jobble.setIcon(x);
         jobble.setActionCommand("jobblentről");
         jobble.addActionListener(kontroller);
         targyak2.add(jobble);
@@ -151,8 +128,8 @@ public class TargyakPanel extends JPanel {
 
         targyak.add(targyak2);
         //Az alkatrész lerak úgy van megírva, hogy bármennyi alkatrészünk van, 1 lerak() hívással a 0. indexűt rakjuk le.  Itt 1 alkatrészt
-        alkatresz.setPreferredSize(new Dimension(100,100));
-        alkatresz.setIcon(new ImageIcon(pisztolyNULL));
+       // alkatresz.setPreferredSize(new Dimension(80, 80));
+        alkatresz.setIcon(pisztolyNULL);
         alkatresz.setActionCommand("lerak");
         alkatresz.addActionListener(kontroller);
         targyak.add(alkatresz);
@@ -160,61 +137,45 @@ public class TargyakPanel extends JPanel {
         targyak.add(targyak2, BorderLayout.LINE_START);
         this.add(targyak);
     }
-    public void update(Jatekos aktivJatekos){
-        LapatVisitor lv=new LapatVisitor();
-        SatorVisitor sv=new SatorVisitor();
-        KotelVisitor kv=new KotelVisitor();
-        AlkatreszVisitor av=new AlkatreszVisitor();
-        fel.setIcon(new ImageIcon(x));
-        jobbfel.setIcon(new ImageIcon(x));
-        jobb.setIcon(new ImageIcon(x));
-        jobble.setIcon(new ImageIcon(x));
-        le.setIcon(new ImageIcon(x));
-        balle.setIcon(new ImageIcon(x));
-        bal.setIcon(new ImageIcon(x));
-        balfel.setIcon(new ImageIcon(x));
-        lapat.setIcon(new ImageIcon(lapatimNULL));
-        sator.setIcon(new ImageIcon(satorimNULL));
-        alkatresz.setIcon(new ImageIcon(pisztolyNULL));
-        kotel.setIcon(new ImageIcon(kotelimNULL));
+
+    public void update(Jatekos aktivJatekos) {
+        LapatVisitor lv = new LapatVisitor();
+        SatorVisitor sv = new SatorVisitor();
+        KotelVisitor kv = new KotelVisitor();
+        AlkatreszVisitor av = new AlkatreszVisitor();
+        fel.setIcon(x);
+        jobbfel.setIcon(x);
+        jobb.setIcon(x);
+        jobble.setIcon(x);
+        le.setIcon(x);
+        balle.setIcon(x);
+        bal.setIcon(x);
+        balfel.setIcon(x);
+        lapat.setIcon(lapatimNULL);
+        sator.setIcon(satorimNULL);
+        alkatresz.setIcon(pisztolyNULL);
+        kotel.setIcon(kotelimNULL);
         //TODO hogy kell összemergelni a képeket helyesen, ha csak két alkatrészem van pl.?
-        boolean vankotel=false;
-        for (Targy t:aktivJatekos.getTargyak()) {
-            if (t.accept(lv))lapat.setIcon(new ImageIcon(lapatim));
-            else if(t.accept(sv))sator.setIcon(new ImageIcon(satorim));
-            else if (t.accept(kv)){kotel.setIcon(new ImageIcon(kotelim));vankotel=true;}
+        boolean vankotel = false;
+        for (Targy t : aktivJatekos.getTargyak()) {
+            if (t.accept(lv)) lapat.setIcon(lapatim);
+            else if (t.accept(sv)) sator.setIcon(satorim);
+            else if (t.accept(kv)) {
+                kotel.setIcon(kotelim);
+                vankotel = true;
+            }
         }
-        Mezo m=aktivJatekos.getTartozkodasiMezo();
+        Mezo m = aktivJatekos.getTartozkodasiMezo();
         if (vankotel) {
-            if (m.getSzomszed(Irany.Fel) != null) fel.setIcon(new ImageIcon(kotelim));
-            if (m.getSzomszed(Irany.JobbFel)!=null)jobbfel.setIcon(new ImageIcon(kotelim));
-            if (m.getSzomszed(Irany.Jobb)!=null)jobb.setIcon(new ImageIcon(kotelim));
-            if (m.getSzomszed(Irany.JobbLe)!=null)jobble.setIcon(new ImageIcon(kotelim));
-            if (m.getSzomszed(Irany.Le)!=null)le.setIcon(new ImageIcon(kotelim));
-            if (m.getSzomszed(Irany.BalLe)!=null)balle.setIcon(new ImageIcon(kotelim));
-            if (m.getSzomszed(Irany.Bal)!=null)bal.setIcon(new ImageIcon(kotelim));
-            if (m.getSzomszed(Irany.BalFel)!=null)balfel.setIcon(new ImageIcon(kotelim));
+            if (m.getSzomszed(Irany.Fel) != null) fel.setIcon(kotelim);
+            if (m.getSzomszed(Irany.JobbFel) != null) jobbfel.setIcon(kotelim);
+            if (m.getSzomszed(Irany.Jobb) != null) jobb.setIcon(kotelim);
+            if (m.getSzomszed(Irany.JobbLe) != null) jobble.setIcon(kotelim);
+            if (m.getSzomszed(Irany.Le) != null) le.setIcon(kotelim);
+            if (m.getSzomszed(Irany.BalLe) != null) balle.setIcon(kotelim);
+            if (m.getSzomszed(Irany.Bal) != null) bal.setIcon(kotelim);
+            if (m.getSzomszed(Irany.BalFel) != null) balfel.setIcon(kotelim);
         }
-        retegek.clear();
-        retegek.add(pisztolyNULL);
-        for (Alkatresz a:aktivJatekos.getAlkatreszek()) {
-            if (a.ID==0)retegek.add(pisztolyegy);
-            if (a.ID==1)retegek.add(pisztolyket);
-            if (a.ID==2)retegek.add(pisztolyhar);
-        }
-
         revalidate();
-    }
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        BufferedImage combined = new BufferedImage(pisztolyegy.getWidth(), pisztolyegy.getHeight(), BufferedImage.TYPE_INT_ARGB);
-
-        Graphics ger = combined.getGraphics();
-        for (BufferedImage img:retegek) {
-            ger.drawImage(img,0,0,null);
-        }
-        ger.dispose();
-        alkatresz.setIcon(new ImageIcon(combined));
     }
 }
